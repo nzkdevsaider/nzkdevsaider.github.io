032b2202409b60e6ecbe30d30bc132f736a08326
Sebastián <sebasmoraresu@gmail.com>
2023-01-05 18:36:57 -0500

    (component) fix <Link>

--- pages/components/SLink.js
+++ pages/components/SLink.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-const SLink = ({ href, light, children }) => {
+const SLink = ({ href, children }) => {
   return (
-    <Link
+    <a
       className="relative hover:before:bottom-0 hover:before:h-full before:absolute before:bg-sblue before:left-[0] before:bottom-1 before:w-full before:h-2 before:-z-[1] before:transition-all before:ease-in-out"
       href={href}
     >
       {children}
-    </Link>
+    </a>
   );
 };
 
