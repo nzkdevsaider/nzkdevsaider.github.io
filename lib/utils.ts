c9bc917a6b1a152509456268e7f8b39257893195
Sebastián <sebasmoraresu@gmail.com>
2024-03-06 16:30:08 -0500

    :sparkles: feat: new utils tools.

---
+++ lib/utils.ts
@@ -0,0 +1,10 @@
+import { ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function generateId(key?: number) {
+	return Math.random().toString(36) + key;
+}
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs))
+}
